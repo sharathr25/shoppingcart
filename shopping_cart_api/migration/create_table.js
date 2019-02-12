a4b24ab27fe47b12747ff6c174065ff31728f1d4
Sharath <sharath>
2019-02-12 15:28:33 +0530

    ADDED readme.txt for popullating database

--- shopping_cart_api/migration/create_table.js
+++ shopping_cart_api/migration/create_table.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 async function createTable() {
   const connection = await mysql.createConnection({
-    host: 'localhost', user: 'shop', password: 'password', database: 'test',
+    host: 'localhost', user: 'shop', password: 'password', database: 'shoppingcart',
   });
   const sql = 'create table products(id int auto_increment primary key,name varchar(50),price int,quantity int,imgurl varchar(500))';
   await connection.execute(sql);
---
+++ shopping_cart_api/migration/readme.txt
@@ -0,0 +1,3 @@
+create one user with username 'shop' and password 'password'
+create one database 'shoppingcart'
+run create_table.js and insert_data.js to popullate data in database
